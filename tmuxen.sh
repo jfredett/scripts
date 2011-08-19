17dc8bb9933c642241c54ee2bff53b6c9d27862a
Joe Fredette <jfredett@gmail.com>
2011-08-19 08:03:29 -0400

    Add spawn/connect commands

--- tmuxen.sh
+++ tmuxen.sh
@@ -4,9 +4,14 @@
 #
 # usage
 #
-#   tmux present? <session>         | exits with code 0 if session present, code 1 otherwise.
-#   tmux absent? <session>          | exiss with code 1 if session present, code 0 otherwise.
-#
+#    present? <session>               | exits with code 0 if session present, code 1
+#                                       otherwise (quiet alias for 'has').
+#    absent? <session>                | exits with code 1 if session present, code 0
+#                                       otherwise.
+#    spawn <session> [-w <script>]    | Spawns a new session without attaching, executes
+#                                       <script> if provided.
+#    connect <session>                | Connects to <session> desynchronously if it
+#                                       exists.
 #
 #
 
@@ -14,11 +19,39 @@
 function tmuxen {
 
 
+  spawn() {
+    local session="$1"
+    local script="$2"
 
+    if tmuxen absent? "$session" ; then
+      tmux new-session -d -s "$session"
+    fi
+
+    [ $VERBOSE ] && echo $script
+
+    [ -n "$script" ] && eval "$script $session"
+  }
+
+  connect() {
+    local session="$1"
+
+    [ $VERBOSE ] && echo $session
+    if tmuxen absent? "$session" ; then
+      echo "$session does not exist, cannot connect"
+    else
+      local inum="-$(tmuxen get-instance-number "$session" inc)"
+      tmux new-session -t "$session" -s "$session$inum"
+    fi
+  }
+
+  get-instance-number() {
+    local curr=$(tmux ls | grep "$1" | sed -n "s/$1-\(.*\): .*/\1/p" | sort -n | tail -n1)
+    [ $VERBOSE ] && echo $2
+    [ -n "$2" ] && echo -n $((curr + 1)) || echo -n "1"
+  }
 
   present() {
-    local session="$1";
-    [[ -n "$(tmux ls 2>/dev/null | grep \"$session\")" ]] && return 0 || return 1
+    tmux has -t "$1" &>/dev/null
   }
 
   absent() {
@@ -30,12 +63,15 @@ function tmuxen {
   case $1 in
     # public
 
-    present?)       present $2             ;;
-    absent?)        absent $2              ;;
+    present?)               present $2                                   ;;
+    absent?)                absent $2                                    ;;
+    spawn)                  spawn $2 $([ "$3" = "-w" ] && echo "$4")     ;;
+    connect)                connect $2                                   ;;
 
     # non-public
 
-    respond-to?)    responds_to $2         ;;
+    respond-to?)            responds_to $2                               ;;
+    get-instance-number)    get-instance-number $2 $3                    ;;
   esac
 }
 
