4a1b7b5681a1b13b3815882640e72ab57478e112
Joe Fredette <jfredett@gmail.com>
2011-08-29 17:43:18 -0400

    refactor and clean up interface

--- clock.sh
+++ clock.sh
@@ -1,10 +1,38 @@
-function clock_in() {
-  echo "in: `date +\"%H:%M:%S %D\"`" >> "$HOME/timesheet"
-}
+function clock {
+  . $HOME/.bash/functions.sh
 
-function clock_out() {
-  [ "$1" != "--for-today" ] && echo "out: `date +\"%H:%M:%S %D\"`" >> "$HOME/timesheet" && return 0
-  echo "" >> "$HOME/timesheet"
-}
+  clock_in() {
+    write "in: `time_now`" 
+  }
 
+  clock_out() {
+   write "out: `time_now`"
+   [ "$1" = "--for-today" ] && write " "
+  }
+
+  write() {
+    echo "$1" >> "$HOME/timesheet"
+  }
+
+  time_now() {
+    date +"%H:%M:%S %D"
+  }
+
+  ps1() {
+    local status=$(tail -n1 "$HOME/timesheet" | sed -n 's/^\([^:]*\):.*/\1/p')
+    echo "$status"
+    [ -z "$status" ] && return
+
+    local color="RED"
+    [ "$status" = "in" ] && color="LGTGRN"
+
+    echo "($(set_color LGTGRN "$status"))"
+  }
+
+  case $1 in
+    in) clock_in ;;
+    out) clock_out $2 ;;
+    ps1) ps1 ;;
+  esac
+}
 
