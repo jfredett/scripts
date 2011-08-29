1f63c9ffebe01ee6343fe9e3cd5c78fecfd02e51
Joe Fredette <jfredett@gmail.com>
2011-08-29 10:07:48 -0400

    more stupid bugs

--- clock.sh
+++ clock.sh
@@ -1,7 +1,7 @@
 function clock_in() {
-  echo "in: `$(date +"%H:%M:%S %D")`" >> "$HOME/timesheet"
+  echo "in: `date +\"%H:%M:%S %D\"`" >> "$HOME/timesheet"
 }
 
 function clock_out() {
-  echo "out: `$(date +"%H:%M:%S %D")`" >> "$HOME/timesheet"
+  echo "out: `date +\"%H:%M:%S %D\"`" >> "$HOME/timesheet"
 }
