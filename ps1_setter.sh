c636bdf5605199c7eeac830a10aef96d2cc078a9
Joe Fredette <jfredett@gmail.com>
2011-09-23 23:50:16 -0400

    refactor ps1-setter to ignore error output when fetching ps1 strings

--- ps1_setter.sh
+++ ps1_setter.sh
@@ -9,21 +9,26 @@ function ps1_setter() {
   source ~/.bash/rvmspy.sh
   source ~/.bash/functions.sh
 
-  function current_dir() {
+  current_dir() {
     echo -n "${PWD#$(echo "$PWD" | xargs dirname | xargs dirname)/}"
   }
 
-  function annoying_mode() {
+  annoying_mode() {
     if [ "$ANNOY_MODE" = "1" ] ; then
       say -v cellos "$(history | tail -n1 | cut -c 8-)" &>/dev/null &
     fi
   }
 
-  function ps1() {
-    local git=$(gitspy ps1)
-    local ruby=$(rvmspy ps1)
-    local stack="$(dirstack ps1)"
-    local clock_state="$(clock ps1)"
+  fetch_ps1() {
+    local ps1_provider="$1"
+    echo $($ps1_provider ps1 2>/dev/null)
+  }
+
+  function ps1 {
+    local git=$(fetch_ps1 gitspy)
+    local ruby=$(fetch_ps1 rvmspy)
+    local stack="$(fetch_ps1 dirstack)"
+    local clock_state="$(fetch_ps1 clock)"
     local current_dir="($(current_dir))"
 
     export PS1=$(squish_spaces "$current_dir $ruby $clock_state $git $stack \n∫∫∫ "; annoying_mode)
