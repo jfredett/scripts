4eb19f1a861090894b6ec778726dd3a27f30d4e0
Joe Fredette <jfredett@gmail.com>
2012-03-07 18:25:57 -0500

    hacking on ultrasub

--- ultrasub.sh
+++ ultrasub.sh
@@ -10,38 +10,39 @@ function ultrasub {
 #2.252s  U
 #8.138s  S
 #1 thread
-function ultrasub_via_exec {
-  find -X "$3" -type f -exec sed -i '' -E "s/$1/$2/" {} \;
-}
+#function ultrasub_via_exec {
+  #find -X "$3" -type f -exec sed -i '' -E "s/$1/$2/" {} \;
+#}
 
 #1.363s R
 #0.782s U
 #0.496s S
 #4 threads
-function ultrasub_via_ack {
-  ack -l "$1" "$3" | xargs -P "${4:=1}" sed -i '' -E "s/$1/$2/"
-}
+#function ultrasub_via_ack {
+  #ack -l "$1" "$3" | xargs -P "${4:=1}" sed -i '' -E "s/$1/$2/"
+#}
 
 #1.566s R
 #0.806s U
 #0.556s S
 #1 thread
-function supersub {
-  ack -l "$1" "$3" | xargs perl -p -i -e "s/$1/$2/"
-}
+#function supersub {
+  #ack -l "$1" "$3" | xargs perl -p -i -e "s/$1/$2/"
+#}
 
 #1.279s R
 #0.772s U
 #0.487s S
 #1 thread
-function supersub_with_sed {
-  ack -l "$1" "$3" | xargs sed -i '' -E "s/$1/$2/"
-}
+#function supersub_with_sed {
+  #ack -l "$1" "$3" | xargs sed -i '' -E "s/$1/$2/"
+#}
 
 #1.228s R
 #0.768s U
 #0.556s S
 #4 threads
-function supersub_proper_with_threads {
-  ack -l "$1" "$3" | xargs -P "${4:=1}" perl -p -i -e "s/$1/$2/"
-}
+#function supersub_proper_with_threads {
+  #ack -l "$1" "$3" | xargs -P "${4:=1}" perl -p -i -e "s/$1/$2/"
+#}
+
